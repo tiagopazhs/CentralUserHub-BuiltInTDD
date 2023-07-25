b0821d1d071f8257fccd3efc49d4340aba08830b
tiagopazhs <tiagopaz.hs@gmail.com>
2023-07-25 19:57:26 -0300

    feat: Write the first test

---
+++ server/tests/unit/repository.spec.ts
@@ -0,0 +1,24 @@
+import UserRepository from "../../src/repository";
+
+describe("UserRepository", () => {
+
+    test("repository should create a new user (C)", async () => {
+
+        const repository = new UserRepository();
+
+        const result = await repository.create({
+            name: "Toretto",
+            email: "toretto@velozesefuriosos.com.br",
+            password: "12345678",
+        });
+
+        // containing just a part of the object without id
+        expect(result).toStrictEqual({
+            name: "Toretto",
+            email: "toretto@velozesefuriosos.com.br",
+            password: "12345678",
+        });
+
+    });
+});
+
