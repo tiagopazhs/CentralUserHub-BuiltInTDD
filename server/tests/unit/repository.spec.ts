73a09b53d10d1fa1648282eca53529415afabb65
tiagopazhs <tiagopaz.hs@gmail.com>
2023-07-29 13:09:37 -0300

    feat: Create default crud methods to save data at db

--- server/tests/unit/repository.spec.ts
+++ server/tests/unit/repository.spec.ts
@@ -1,25 +1,46 @@
 import {describe, expect, test} from '@jest/globals';
+import { MongoClient } from "mongodb";
 import UserRepository from "../../src/repository";
 
 describe("UserRepository", () => {
 
-    test("repository should create a new user (C)", async () => {
+    let repository;
+    let client;
 
-        const repository = new UserRepository();
+    beforeAll(async () => {
+        const dsn = 'mongodb://root:root@localhost?retryWrites=true&writeConcern=majority'
+        client = new MongoClient(dsn);
+        await client.connect();
+        const collection = client.db('app_db').collection('events');
+        repository = UserRepository(collection);
+    });
+
+
+    //close db connection after all
+    afterAll(async () => {
+        await client.close();
+    });
+
+    beforeEach(async () => {
+        await repository.deleteAll();
+    });
+
+    test("repository should create a new event (C)", async () => {
 
         const result = await repository.create({
-            name: "Toretto",
-            email: "toretto@velozesefuriosos.com.br",
-            password: "12345678",
-        });
+            name: 'Rock in Rio',
+            date: '2024-02-07'
+        })
 
-        // containing just a part of the object without id
-        expect(result).toStrictEqual({
-            name: "Toretto",
-            email: "toretto@velozesefuriosos.com.br",
-            password: "12345678",
-        });
+        //containing just a part of the object 
+        expect(result).toStrictEqual(expect.objectContaining({
+            name: 'Rock in Rio',
+            date: '2024-02-07'
+        }));
 
-    });
+        const events = await repository.findAll();
+
+        expect(events.length).toBe(1);
+
+    })
 });
-
