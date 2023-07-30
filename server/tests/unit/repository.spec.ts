4721cd51baff82037ebf733f46da019260a90a71
tiagopazhs <tiagopaz.hs@gmail.com>
2023-07-30 14:28:43 -0300

    feat: Add read method and beforeEach

--- server/tests/unit/repository.spec.ts
+++ server/tests/unit/repository.spec.ts
@@ -2,43 +2,67 @@ import { describe, expect, test } from '@jest/globals';
 import UserRepository from "../../src/repository";
 
 describe("UserRepository", () => {
-  test('repository should create a new user (C)', async () => {
-    const user = {
-      name: 'Renato',
-      email: 'contato.unit@legiaourbana.com',
-      password: 'senha123',
-    };
+
+  const user = {
+    name: 'Renato',
+    email: 'contato.unit@legiaourbana.com',
+    password: 'senha123',
+  };
+
+  let userRepo;
+  let mockCollection;
+
+  beforeEach(async () => {
 
     // Mock object
-    const mockInsertOne = jest.fn(() => Promise.resolve({ ops: [user] }));
+    const mockInsertOne = jest.fn(() => {
+      mockFind.mockImplementation(() => ({
+        toArray: () => Promise.resolve([user]),
+      }));
+
+      return Promise.resolve({ ops: [user] })
+    });
     const mockFind = jest.fn(() => ({
-      toArray: () => Promise.resolve([user]),
+      toArray: () => Promise.resolve([]),
     }));
-    const mockCollection = {
+    mockCollection = {
       insertOne: mockInsertOne,
       find: mockFind,
     };
-
-    const userRepo = UserRepository(mockCollection);
-
-    const result = await userRepo.create(user);
-
-    expect(result).toEqual(user);
-
-    // Spy 'Espião'
-    expect(mockInsertOne).toHaveBeenCalledWith(user);
+    userRepo = UserRepository(mockCollection);
 
   });
 
+  test('repository should create a new user (C)', async () => {
+
+    // 1 -> Db must be empty.
+    expect((await userRepo.findAll()).length).toBe(0);
+
+    // 2 -> User created with success.
+    const result = await userRepo.create(user);
+    expect(result).toEqual(user);
+
+    // 3 -> Db must have one record.
+    expect((await userRepo.findAll()).length).toBe(1);
+
+    // extra -> Spy 'Espião'
+    expect(mockCollection.insertOne).toHaveBeenCalledWith(user);
+
+  });
+
+
   test('Respository must read an user (R)', async () => {
 
+    // 1 -> Db must be empty.
+    expect((await userRepo.findAll()).length).toBe(0);
 
-    // 1. Db must be empty.
+    // 2 -> User created with success.
+    const result = await userRepo.create(user);
+    expect(result).toEqual(user);
 
-    // 2. Insert an user.
-
-    // 3. Test if the user was update with success.
-
+    // 3 -> First record must be equal the user object.
+    expect((await userRepo.findAll())[0]).toStrictEqual(user);
+    
   });
 
   test('Respository must update an user (U)', async () => {
