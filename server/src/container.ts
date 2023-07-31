62af3364a1223b9fd32239f19d8758b299b118e9
tiagopazhs <tiagopaz.hs@gmail.com>
2023-07-30 21:49:25 -0300

    feat: Create container function

---
+++ server/src/container.ts
@@ -0,0 +1,35 @@
+import { MongoClient } from "mongodb";
+
+interface Container {
+    getClient: () => Promise<MongoClient>;
+}
+
+interface Services {
+    client: MongoClient | null;
+}
+
+
+async function createContainer(): Promise<Container> {
+    const services: Services = {
+        client: null,
+    };
+
+    async function getClient(): Promise<MongoClient> {
+        if (services.client) {
+            return services.client;
+        }
+
+        const dsn = 'mongodb://root:root@localhost?retryWrites=true&writeConcern=majority';
+        const client = new MongoClient(dsn);
+        await client.connect();
+
+        services.client = client;
+        return client;
+    }
+
+    return {
+        getClient,
+    };
+}
+
+export default createContainer;
