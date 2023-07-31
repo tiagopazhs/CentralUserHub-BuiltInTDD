1eae047cd96b0c018aef8f38a0309c9f159b3024
tiagopazhs <tiagopaz.hs@gmail.com>
2023-07-30 23:16:13 -0300

    fix: Add type null to empty repository

--- server/src/container.ts
+++ server/src/container.ts
@@ -2,52 +2,51 @@ import { MongoClient } from "mongodb";
 import UserRepository, { UserRepositoryInterface } from "./repository";
 
 interface Container {
-    getClient: () => Promise<MongoClient>;
-    getRepository: () => Promise<UserRepositoryInterface>;
+  getClient: () => Promise<MongoClient>;
+  getRepository: () => Promise<UserRepositoryInterface | null>;
 }
 
 interface Services {
-    client: MongoClient | null;
-    repository: UserRepositoryInterface
+  client: MongoClient | null;
+  repository: UserRepositoryInterface | null;
 }
 
-
 async function createContainer(): Promise<Container> {
-    const services: Services = {
-        client: null,
-        repository: null,
-    };
+  const services: Services = {
+    client: null,
+    repository: null,
+  };
 
-    async function getClient(): Promise<MongoClient> {
-        if (services.client) {
-            return services.client;
-        }
-
-        const dsn = 'mongodb://root:root@localhost?retryWrites=true&writeConcern=majority';
-        const client = new MongoClient(dsn);
-        await client.connect();
-
-        services.client = client;
-        return client;
+  async function getClient(): Promise<MongoClient> {
+    if (services.client) {
+      return services.client;
     }
 
-    async function getRepository(): Promise<UserRepositoryInterface> {
-        if (services.repository !== undefined && services.repository !== null) {
-            return services.repository;
-        }
+    const dsn = 'mongodb://root:root@localhost?retryWrites=true&writeConcern=majority';
+    const client = new MongoClient(dsn);
+    await client.connect();
 
-        const client = await getClient();
+    services.client = client;
+    return client;
+  }
 
-        await client.connect();
-        const collection = client.db('app_db').collection('events');
-
-        return (services.repository = UserRepository(collection));
+  async function getRepository(): Promise<UserRepositoryInterface> {
+    if (services.repository !== undefined && services.repository !== null) {
+      return services.repository;
     }
 
-    return {
-        getClient,
-        getRepository
-    };
+    const client = await getClient();
+
+    await client.connect();
+    const collection = client.db('app_db').collection('events');
+
+    return (services.repository = UserRepository(collection));
+  }
+
+  return {
+    getClient,
+    getRepository
+  };
 }
 
 export default createContainer;
