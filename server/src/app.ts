60fe7290c5af7c9e9f24d09ee6f3127a8c302952
tiagopazhs <tiagopaz.hs@gmail.com>
2023-07-31 23:35:25 -0300

    feat: add default app server

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,18 @@
-import express, { Request, Response } from 'express'; // Make sure to import Request and Response types
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import createContainer from './container';
 
 const app = express();
+app.use(express.json());
+app.use(cors({
+  exposedHeaders: ['x-total-count'],
+}));
+app.set('container', createContainer());
+
+const normalizePk = (user) => {
+  user._id = user._id;
+  return user;
+};
 
 app.get('/users', async (request: Request, response: Response) => {
   response.json({ message: 'hello world' }); // Wrap the string in an object
