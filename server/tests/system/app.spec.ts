97b6c055f1ab9c0f27fd6ba3ad61f06dd2738840
tiagopazhs <tiagopaz.hs@gmail.com>
2023-08-05 17:34:20 -0300

    config: Commit local changes

--- server/tests/system/app.spec.ts
+++ server/tests/system/app.spec.ts
@@ -2,6 +2,7 @@ import { ObjectId } from 'mongodb';
 import app from '../../src/app';
 import Container from '../../src/container';
 import supertest from 'supertest';
+const request = supertest(app)
 
 describe('Event Management API', () => {
 
@@ -28,19 +29,53 @@ describe('Event Management API', () => {
         await repository.deleteAll();
     });
 
-    describe('Endpoints de coleção', () => {
+    describe('Colection endpoints', () => {
         test('GET /users', async () => {
-            await repository.create(user);
+            console.log('user->', user)
+            const userCreate = await repository.create(user);
+            // format user._id
+            userCreate._id = userCreate._id.toHexString();
+            console.log('user->', user)
 
-            const response = await supertest(app)
+            const response = await request
                 .get('/users')
                 .expect('Content-type', /application\/json/);
 
             expect(response.statusCode).toBe(200);
             expect(response.body.length).toBe(1);
+
+
+            // const expectedId = new ObjectId(response.body[0]._id);
+            // console.log('response.body[0]-->', expectedId)
+            // console.log('response.body[0]-->', response.body[0])
+            // console.log('user-->', user)
+
+            // format user._id
+            const valor = userCreate._id.toHexString();
+            console.log('valor-->', valor);
+
             expect(response.body[0].email).toStrictEqual(user.email);
             expect(response.body[0].name).toStrictEqual(user.name);
             expect(response.body[0].password).toStrictEqual(user.password);
+            // expect(response.body[0]).toStrictEqual(expect.objectContaining(user));
         });
+
+        // test('POST /users', async () => {
+
+        //     const response = await supertest(app)
+        //         .post('/users')
+        //         .send(user);
+
+        //         console.log('-->', response.body)
+        //         console.log('-->', user)
+
+        //         const expectedUser = { ...user, _id: new ObjectId(user.id) };
+        //         expect(response.body).toStrictEqual([expect.objectContaining(expectedUser)]);
+            
+        //         console.log('Expected:', expectedUser);
+        //         console.log('Received:', response.body);
+        // });
+
     });
+    
 });
