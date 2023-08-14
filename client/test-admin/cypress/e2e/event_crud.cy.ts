8e3e515aec6fa73ac6d4dfe5337e61dbeb4398d7
tiagopazhs <tiagopaz.hs@gmail.com>
2023-08-13 22:22:15 -0300

    feat: Write cypress test to create an user

--- client/test-admin/cypress/e2e/event_crud.cy.ts
+++ client/test-admin/cypress/e2e/event_crud.cy.ts
@@ -4,5 +4,18 @@ describe('Event crud', () => {
     cy.contains('Kiko')
   });
 
+  it.only('Should create an user', () => {
+    cy.visit('http://localhost:5173');
+    cy.get('.RaCreateButton-root').click();
+    cy.get('#name').type('Kiko Zambianchi');
+    cy.get('#email').type('contato@capital.com');
+    cy.get('#password').type('dinho@123');
+    cy.get('.MuiInputAdornment-root > .MuiButtonBase-root').click();
+    cy.get('.RaToolbar-defaultToolbar > .MuiButtonBase-root').click();
+    cy.contains('created');
+    cy.visit('http://localhost:5173');
+    cy.contains('Kiko Zambianchi');
+    cy.contains('contato@capital.com');
+  });
 
 })
