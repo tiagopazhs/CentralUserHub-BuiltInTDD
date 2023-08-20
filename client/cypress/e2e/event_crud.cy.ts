b7b044bb8d125b738e54c44d66e52e871c9af741
tiagopazhs <tiagopaz.hs@gmail.com>
2023-08-19 21:01:18 -0300

    feat: Create test  when delete an event

--- client/cypress/e2e/event_crud.cy.ts
+++ client/cypress/e2e/event_crud.cy.ts
@@ -1,10 +1,11 @@
 describe('Event crud', () => {
   it('Should list all users', () => {
+    cy.task('db:erase');
     cy.visit('http://localhost:5173');
     cy.contains('Kiko')
   });
 
-  it.only('Should create an user', () => {
+  it('Should create an user', () => {
     cy.visit('http://localhost:5173');
     cy.get('.RaCreateButton-root').click();
     cy.get('#name').type('Kiko Zambianchi');
@@ -18,4 +19,60 @@ describe('Event crud', () => {
     cy.contains('contato@capital.com');
   });
 
+  it.only('Should delete an user', () => {
+    cy.visit('http://localhost:5173');
+    cy.get('.MuiTableBody-root > :nth-child(1) > .column-id').click();
+    cy.get('.MuiButton-text').click();
+    cy.contains('Element deleted')
+  });
+
 })
+
+// describe('Event CRUD', () => {
+
+//   beforeEach(() => {
+//     cy.task('db:erase');
+//     cy.visit('http://localhost:3000');
+//   });
+
+//   it('Deve listar todos os eventos', () => {
+//     cy.task('db:create', {
+//       name: 'Rock in Rio',
+//       date: '2024-01-01'
+//     });
+//     cy.contains('Rock in Rio');
+//   });
+
+//   it('Deve criar um novo evento', () => {
+//     cy.get('.RaCreateButton-root').click();
+//     cy.get('#name').type('AWS Summit');
+//     cy.get('#date').type('2023-03-03');
+//     cy.get('.RaToolbar-defaultToolbar > .MuiButtonBase-root').click();
+//     cy.contains('Element created');
+//     cy.visit('http://localhost:3000');
+//     cy.contains('AWS Summit');
+//   });
+
+//   it('Deve remover um evento', () => {
+//     cy.task('db:create', {
+//       name: 'Rock in Rio',
+//       date: '2024-01-01'
+//     });
+//     cy.get('.MuiTableBody-root > :nth-child(1)').click();
+//     cy.get('.ra-delete-button').click();
+//     cy.contains('Element deleted');
+//   });
+
+//   it('Deve atualizar um evento', () => {
+//     cy.task('db:create', {
+//       name: 'Rock in Rio',
+//       date: '2024-01-01'
+//     });
+//     cy.get('.MuiTableBody-root > :nth-child(1)').click();
+//     cy.get('#name').clear().type('AWS Summit');
+//     cy.get('#date').clear().type('2024-03-03');
+//     cy.get('.RaToolbar-defaultToolbar > .MuiButton-contained').click();
+//     cy.contains('Element updated');
+//     cy.contains('AWS Summit');
+//   });
+// });
